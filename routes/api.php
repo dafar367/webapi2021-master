7732a2dfd5b50ddbc8c88eeb136488642afbe4e6
Your Name <you@example.com>
2021-11-18 13:43:07 +0700

    master

---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CourseController;
+use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\Auth\loginController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('register', [RegisterController::class, 'register']);
+
+Route::post('login', [LoginController::class, 'login']);
+Route::post('refresh', [LoginController::class, 'refresh']);
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::apiResource('courses', CourseController::class);
+    Route::post('logout', [LoginController::class, 'logout']);
+
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
